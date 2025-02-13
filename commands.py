0cb5bdb7b6de5198cb1a28ee6e723ca6fd3d527b
sigmanor <sigmanor@gmail.com>
2025-02-13 21:13:36 +0200

    feat: add GitHub link to bot's start command message

--- commands.py
+++ commands.py
@@ -24,7 +24,8 @@ async def start_command(update: Update, context) -> None:
         f"Привіт 🏴‍☠️\n\nЯ покажу тобі випадковий фільм/серіал/мультфільм з сайту <a href='https://{uakino_url}'>uakino</a>\n\n"
         "<b>Список команд:</b>\n/movie <i>фільм</i>\n/serial <i>серіал</i>\n/cartoon <i>мультфільм</i>\n\n"
         f"<b>Версія бота:</b> {app_version}\n\n"
-        f"<blockquote>Бот був створений задля розваги і немає ніякого зв'язку з сайтом https://{uakino_url} як і <a href='https://t.me/sigmanor'>автор</a> бота</blockquote>",
+        f"<blockquote>Бот був створений задля розваги і немає ніякого зв'язку з сайтом https://{uakino_url} як і <a href='https://t.me/sigmanor'>автор</a> бота</blockquote>\n\n"
+        f"Вихідний код бота можна знайти на  <a href='https://github.com/Sigmanor/uakino.club_bot'>GitHub</a>\n",
         disable_web_page_preview=True,
         parse_mode=ParseMode.HTML,
     )
